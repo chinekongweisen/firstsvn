b37ff43dce89d2703ba01ac16b0e5250b8a35b80
aa <aa>
2018-02-07 23:02:09 +0800

    第一次提交

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test {
+    int x = 99;
+    public void print(){}
+    public void setX(int x) {
+        this.x=x;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
